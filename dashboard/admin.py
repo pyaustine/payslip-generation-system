c298b3f623eb7ed095144004828930dd4c3ccb64
sphinx-austin <austinjuniour4@gmail.com>
2023-11-10 22:08:30 +0300

    register staff model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Grade
+from .models import Grade, Staff
 
 
 # Register your models here.
 class GradeAdmin(admin.ModelAdmin):
     list_display = ('position', 'basic_salary')
 
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'gross_salary')
+
 admin.site.register(Grade, GradeAdmin)
+admin.site.register(Staff, StaffAdmin)
