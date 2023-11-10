df4ddf96231b4e1f1ff291974c16370dea4062e9
sphinx-austin <austinjuniour4@gmail.com>
2023-11-10 21:55:05 +0300

    model migrations

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Grade
+
 
 # Register your models here.
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('position', 'basic_salary')
+
+admin.site.register(Grade, GradeAdmin)
