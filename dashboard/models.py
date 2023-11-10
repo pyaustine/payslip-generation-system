b4e6a1a9fcdc81b80099ba751199b5f5b1ff0366
sphinx-austin <austinjuniour4@gmail.com>
2023-11-10 21:42:52 +0300

    add Grade model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Grade(models.Model):
+    position = models.CharField(max_length=100)
+    basic_salary = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.position
