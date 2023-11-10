50c08ad5238b954da7ba59c13e78764d7242a106
sphinx-austin <austinjuniour4@gmail.com>
2023-11-10 22:05:59 +0300

    add Staff model

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,3 +7,18 @@ class Grade(models.Model):
 
     def __str__(self):
         return self.position
+    
+class Staff(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    hours = models.PositiveIntegerField(default=0)
+    grade_status = models.ForeignKey(Grade, on_delete=models.CASCADE)
+    gross_salary = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        self.gross_salary = (self.hours * self.grade_status.basic_salary)
+        return super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return self.first_name
