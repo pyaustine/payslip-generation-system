6b1ecbccc25e2fd4bedb7af40e2767e6c77b3081
sphinx-austin <austinjuniour4@gmail.com>
2023-11-13 22:01:41 +0300

    update

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . models import Staff
 
 # Create your views here.
+def index(request):
+    staff = Staff.objects.all()
+
+    context = {
+        'staff': staff
+    }
+    return render(request, 'dashboard/index.html', context)
